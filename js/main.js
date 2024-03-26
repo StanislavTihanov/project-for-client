bc40fa753f0759eed9f5766d32b157c7ce09c7d9
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-03-26 09:48:16 +0300

    Add page vacancy

--- js/main.js
+++ js/main.js
@@ -21,14 +21,13 @@ window.addEventListener('scroll', () => {
 })
 //------------------------------------------------------------------------появление бекграунда у шапки
 
+//------------------------------------------------------------------------выпадающий список у шапки
 const buttonList = document.querySelector('.menu__list-item--mod');
 const list = document.querySelector('.categories__list');
  buttonList.addEventListener ('click', () => {
     list.classList.toggle('open-list')
  })
-
-
-
+//------------------------------------------------------------------------выпадающий список у шапки
 
 //------------------------------------------------------------------------Меню-Бургер
 const burgerMenu = document.querySelector('.header__burger');
@@ -453,31 +452,28 @@ if (menuLinks.length > 0) {
 //------------------------------------------------------------------------popup
 
 //------------------------------------------------------------------------Accordion
-//const titles = document.querySelectorAll('.accordion__title');
-//const contents = document.querySelectorAll('.accordion__content');
-//
-//titles.forEach(item => item.addEventListener('click', () => {
-//    const activeContent = document.querySelector('#' + item.dataset.tab);
-//
-//    if (activeContent.classList.contains('active')) {
-//        activeContent.classList.remove('active');
-//        item.classList.remove('active');
-//        activeContent.style.maxHeight = 0;
-//    } else {
-//      contents.forEach(element => {
-//        element.classList.remove('active');
-//        element.style.maxHeight = 0;
-//      });
-//      titles.forEach(element => element.classList.remove('active'));
-//
-//      item.classList.add('active');
-//      activeContent.classList.add('active');
-//      activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
-//    }
-//}));
-//document.querySelector('[data-tab="tab-1').classList.add('active');
-//document.querySelector('#tab-1').classList.add('active');
-//document.querySelector('#tab-1').style.maxHeight = document.querySelector('#tab-1').scrollHeight + 'px';
+const titles = document.querySelectorAll('.accordion__title');
+const contents = document.querySelectorAll('.accordion__content');
+
+titles.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab);
+
+    if (activeContent.classList.contains('active')) {
+        activeContent.classList.remove('active');
+        item.classList.remove('active');
+        activeContent.style.maxHeight = 0;
+    } else {
+      contents.forEach(element => {
+        element.classList.remove('active');
+        element.style.maxHeight = 0;
+      });
+      titles.forEach(element => element.classList.remove('active'));
+
+      item.classList.add('active');
+      activeContent.classList.add('active');
+      activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+}));
 //------------------------------------------------------------------------Accordion
 
 
